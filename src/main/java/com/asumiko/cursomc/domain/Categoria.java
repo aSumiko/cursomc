aaec49bd1c5f339a5e58e4e32e8e00b245c94abd
aSumiko <ana.sumiko@gmail.com>
2022-11-22 15:31:10 -0300

    Criando reository e service para Categoria

--- src/main/java/com/asumiko/cursomc/domain/Categoria.java
+++ src/main/java/com/asumiko/cursomc/domain/Categoria.java
@@ -10,7 +10,6 @@ import javax.persistence.Id;
 
 @Entity
 public class Categoria implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -45,7 +44,10 @@ public class Categoria implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -57,7 +59,12 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (! id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
